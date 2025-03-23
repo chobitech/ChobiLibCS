290746a5cc33e63ec87b66ae9847b2cb449d85df
Tadayoshi Fujiki <chobitech.g@gmail.com>
2025-03-23 20:27:20 +0900

    1

---
+++ ChobiLib.cs
@@ -0,0 +1,16 @@
+﻿namespace ChobiLib;
+
+using System;
+
+public static class ChobiLib
+{
+
+    public static T Also<T>(this T t, Action<T> action)
+    {
+        action(t);
+        return t;
+    }
+
+    public static R Let<T, R>(this T t, Func<T, R> func) => func(t);
+
+}
