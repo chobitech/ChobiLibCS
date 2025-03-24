89366d9b0d5a3ec3136c73725bc7f0acbeb9f886
chobitech <chobitech.g@gmail.com>
2025-03-24 13:38:59 +0900

    11

--- RandomString.cs
+++ RandomString.cs
@@ -7,15 +7,15 @@ public class RandomString(IList<string> seeds, Random? random = null) : Randomiz
     private const string lowerNonHexString = "ghijklmnopqrstuvwxyz";
 
 
-    public static RandomString GetDigitsOnlyRandomString(Random? random) => new(digits, random);
+    public static RandomString GetDigitsOnlyRandomString(Random? random = null) => new(digits, random);
 
-    public static RandomString GetLowerHexRandomString(Random? random) => new(digits + lowerHexString, random);
-    public static RandomString GetUpperHexRandomString(Random? random) => new(digits + lowerHexString.ToUpper(), random);
-    public static RandomString GetHexRandomString(Random? random) => new(digits + lowerHexString + lowerHexString.ToUpper(), random);
+    public static RandomString GetLowerHexRandomString(Random? random = null) => new(digits + lowerHexString, random);
+    public static RandomString GetUpperHexRandomString(Random? random = null) => new(digits + lowerHexString.ToUpper(), random);
+    public static RandomString GetHexRandomString(Random? random = null) => new(digits + lowerHexString + lowerHexString.ToUpper(), random);
 
-    public static RandomString GetLowerCharDigitsRandomString(Random? random) => new(digits + lowerHexString + lowerNonHexString, random);
-    public static RandomString GetUpperCharDigitsRandomString(Random? random) => new(digits + (lowerHexString + lowerNonHexString).ToUpper(), random);
-    public static RandomString GetCharDigitsRandomString(Random? random) => (lowerHexString + lowerNonHexString).Let(cStr => new RandomString(digits + cStr + cStr.ToUpper(), random));
+    public static RandomString GetLowerCharDigitsRandomString(Random? random = null) => new(digits + lowerHexString + lowerNonHexString, random);
+    public static RandomString GetUpperCharDigitsRandomString(Random? random = null) => new(digits + (lowerHexString + lowerNonHexString).ToUpper(), random);
+    public static RandomString GetCharDigitsRandomString(Random? random = null) => (lowerHexString + lowerNonHexString).Let(cStr => new RandomString(digits + cStr + cStr.ToUpper(), random));
 
 
     public RandomString(string s, Random? random = null) : this(s.ToStringList(), random)
