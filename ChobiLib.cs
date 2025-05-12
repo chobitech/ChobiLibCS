4dad6be2f2baf048adbc7f93e2e16a34a8d3f6a0
chobitech <chobitech@users.noreply.github.com>
2025-05-12 15:36:31 +0900

    19

--- ChobiLib.cs
+++ ChobiLib.cs
@@ -1,4 +1,5 @@
-﻿using System.Numerics;
+﻿using System.ComponentModel;
+using System.Numerics;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -30,6 +31,21 @@ public static class ChobiLib
 
     public static bool IsNotEmpty(this StringBuilder sb) => sb.Length > 0;
 
+    public static string JoinToString<T>(this IEnumerable<T> values, string joint = "", Func<T, string>? converter = null)
+    {
+        var sb = new StringBuilder();
+        foreach (var v in values)
+        {
+            if (joint != null && joint != "" && sb.IsNotEmpty())
+            {
+                sb.Append(joint);
+            }
+            sb.Append(v);
+        }
+        return sb.ToString();
+    }
+
+    /*
     public static string JoinToString<T>(this IList<T> list, string joint = "", Func<T, string>? converter = null)
     {
         var sb = new StringBuilder();
@@ -43,6 +59,7 @@ public static class ChobiLib
         }
         return sb.ToString();
     }
+    */
 
     public static T Replace<T>(this IList<T> list, int index, T value)
     {
@@ -56,35 +73,35 @@ public static class ChobiLib
         (list[index2], list[index1]) = (list[index1], list[index2]);
     }
 
-    public static R FoldIndexed<T, R>(this IList<T> list, R initialValue, Func<R, T, int, R> func)
+    public static R FoldIndexed<T, R>(this IEnumerable<T> list, R initialValue, Func<R, T, int, R> func)
     {
         var res = initialValue;
 
-        for (var i = 0; i < list.Count; i++)
+        for (var i = 0; i < list.Count(); i++)
         {
-            res = func(res, list[i], i);
+            res = func(res, list.ElementAt(i), i);
         }
 
         return res;
     }
 
-    public static R Fold<T, R>(this IList<T> list, R initialValue, Func<R, T, R> func) => list.FoldIndexed(initialValue, (res, v, _) => func(res, v));
+    public static R Fold<T, R>(this IEnumerable<T> list, R initialValue, Func<R, T, R> func) => list.FoldIndexed(initialValue, (res, v, _) => func(res, v));
 
-    public static void ForeachIndexed<T>(this IList<T> list, Action<T, int> action)
+    public static void ForeachIndexed<T>(this IEnumerable<T> list, Action<T, int> action)
     {
-        for (var i = 0; i < list.Count; i++)
+        for (var i = 0; i < list.Count(); i++)
         {
-            action(list[i], i);
+            action(list.ElementAt(i), i);
         }
     }
 
-    public static List<R> MapIndexed<T, R>(this IList<T> list, Func<T, int, R> func, bool ignoreNull = false) where T : class
+    public static List<R> MapIndexed<T, R>(this IEnumerable<T> list, Func<T, int, R> func, bool ignoreNull = false) where T : class
     {
         var res = new List<R>();
 
-        for (var i = 0; i < list.Count; i++)
+        for (var i = 0; i < list.Count(); i++)
         {
-            var v = list[i];
+            var v = list.ElementAt(i);
             var r = func(v, i);
             if (!ignoreNull || v != null)
             {
@@ -95,22 +112,22 @@ public static class ChobiLib
         return res;
     }
 
-    public static List<R> Map<T, R>(this IList<T> list, Func<T, R> func, bool ignoreNull = false) where T : class => list.MapIndexed((v, _) => func(v), ignoreNull);
+    public static List<R> Map<T, R>(this IEnumerable<T> list, Func<T, R> func, bool ignoreNull = false) where T : class => list.MapIndexed((v, _) => func(v), ignoreNull);
 
-    public static List<R> MapIndexed<T, R>(this IList<T> list, Func<T, int, R> func) where T : struct
+    public static List<R> MapIndexed<T, R>(this IEnumerable<T> list, Func<T, int, R> func) where T : struct
     {
         var res = new List<R>();
 
-        for (var i = 0; i < list.Count; i++)
+        for (var i = 0; i < list.Count(); i++)
         {
-            res.Add(func(list[i], i));
+            res.Add(func(list.ElementAt(i), i));
 
         }
 
         return res;
     }
 
-    public static List<R> Map<T, R>(this IList<T> list, Func<T, R> func) where T : struct => list.MapIndexed((v, _) => func(v));
+    public static List<R> Map<T, R>(this IEnumerable<T> list, Func<T, R> func) where T : struct => list.MapIndexed((v, _) => func(v));
 
 
     public static T[] MergeArrays<T>(params T[][] arrays)
@@ -203,19 +220,6 @@ public static class ChobiLib
 
         return [.. res];
     }
-
-    /*
-    private static T InnerCircularAdd<T>(this T v, T addValue, T maxInclusive, T startValue) where T : System.Numerics.INumber<T>
-    {
-        var res = v + addValue;
-        if (res > maxInclusive)
-        {
-            res = startValue;
-        }
-        return res;
-    }
-    */
-
 
     public static long CircularAdd(this long v, long addValue, long maxInclusive, long startValue = 0)
     {
--- NumericUnit.cs
+++ NumericUnit.cs
@@ -1,6 +1,17 @@
 using System.Numerics;
+using System.Text;
 
 namespace ChobiLib;
+
+
+
+public static class ChobiBigIntegerExtensions
+{
+    public static BigInteger Round(this BigInteger bi, int digits) => NumericUnit.GetRoundedBigInteger(bi, digits);
+    public static BigInteger Ceil(this BigInteger bi, int digits) => NumericUnit.GetCeilingBigInteger(bi, digits);
+    public static BigInteger Floor(this BigInteger bi, int digits) => NumericUnit.GetFlooringBigInteger(bi, digits);
+}
+
 
 public class NumericUnit
 {
@@ -10,19 +21,18 @@ public class NumericUnit
 
 
     private static readonly Dictionary<int, BigInteger> digitsAndBaseValueMap = [];
+
     private static BigInteger GetBaseValue(int digits)
     {
-        if (digits < 3)
+        lock (digitsAndBaseValueMap)
         {
-            return BigInteger.One;
+            if (!digitsAndBaseValueMap.TryGetValue(digits, out BigInteger bv))
+            {
+                bv = BigInteger.Pow(10, digits);
+                digitsAndBaseValueMap[digits] = bv;
+            }
+            return bv;
         }
-
-        if (!digitsAndBaseValueMap.TryGetValue(digits, out BigInteger bv))
-        {
-            bv = BigInteger.Pow(10, digits);
-            digitsAndBaseValueMap[digits] = bv;
-        }
-        return bv;
     }
 
 
@@ -32,6 +42,173 @@ public class NumericUnit
     {
         public string Label { get; } = label;
         public string Reading { get; } = reading;
+    }
+
+    private static readonly Dictionary<int, string> jpNumberMap = new()
+    {
+        { 0, "〇" },
+        { 1, "一" },
+        { 2, "二" },
+        { 3, "三" },
+        { 4, "四" },
+        { 5, "五" },
+        { 6, "六" },
+        { 7, "七" },
+        { 8, "八" },
+        { 9, "九" }
+    };
+
+    private static string InnerGetJpUnitNumber(int value, int baseValue, string suffix)
+    {
+        if (value < baseValue)
+        {
+            return "";
+        }
+
+        var p = value / baseValue;
+        if (p > 1)
+        {
+            return jpNumberMap[p] + suffix;
+        }
+        else
+        {
+            return suffix;
+        }
+    }
+
+    private static string InnerGet4DigitsJpNumberString(int n)
+    {
+        if (n >= 10000)
+        {
+            throw new Exception("n over 10000");
+        }
+
+        var s = InnerGetJpUnitNumber(n, 1000, "千");
+        n %= 1000;
+
+        s += InnerGetJpUnitNumber(n, 100, "百");
+        n %= 100;
+
+        s += InnerGetJpUnitNumber(n, 10, "十");
+        n %= 10;
+
+        s += jpNumberMap[n];
+
+        return s;
+    }
+
+    private static List<string> InnerGetJpNumberUnitList(BigInteger bi)
+    {
+        var iList = new List<string>();
+        do
+        {
+            var partInt = (int)(bi % jpUnitBase);
+            iList.Add(InnerGet4DigitsJpNumberString(partInt));
+            bi /= jpUnitBase;
+        } while (bi > BigInteger.Zero);
+
+        return iList;
+    }
+
+    
+    private static string GetShortenJpNumberString(BigInteger bi)
+    {
+        var sb = new StringBuilder();
+
+        if (bi < jpUnitBase)
+        {
+            sb.Append(InnerGet4DigitsJpNumberString((int)bi));
+        }
+        else
+        {
+            var unitCount = jpUnitMap.Count;
+            var lastUnitData = jpUnitMap.Last().Value!.Value;
+            var last10Exp = jpUnitMap.Last().Key;
+
+            var bi10Exp = GetBase10Exponent(bi);
+            var exp10 = bi10Exp - (bi10Exp % 4);
+            var roundBi = bi.Round(exp10 -1);
+
+            var unitDigits = bi10Exp % last10Exp;
+            var unitStr = "";
+            for (var i = unitCount - 1; i > 0; i--)
+            {
+                var elm = jpUnitMap.ElementAt(i);
+                if (unitDigits >= elm.Key && elm.Value != null)
+                {
+                    unitStr = elm.Value.Value.Label;
+                    break;
+                }
+            }
+
+            for (var i = 0; i < bi10Exp / last10Exp; i++)
+            {
+                unitStr += lastUnitData.Label;
+            }
+
+            var exp10BaseValue = GetBaseValue(exp10);
+            var iPart = InnerGet4DigitsJpNumberString((int)(roundBi / exp10BaseValue));
+            var fPart = (int)((roundBi % exp10BaseValue) / GetBaseValue(exp10 - 1));
+
+            sb.Append(iPart).Append("点").Append(jpNumberMap[fPart]).Append(unitStr);
+        }
+
+
+
+        return sb.ToString();
+    }
+
+    public static string GetJpNumberString(BigInteger bi, bool isShorten = false)
+    {
+        if (isShorten)
+        {
+            return GetShortenJpNumberString(bi);
+        }
+
+        var numList = InnerGetJpNumberUnitList(bi);
+        
+        if (numList.Count == 0)
+        {
+            return "";
+        }
+        
+        var sb = new StringBuilder();
+
+        var unitCount = jpUnitMap.Count;
+        var lastNumData = jpUnitMap.Last().Value!.Value;
+
+        do
+        {
+            List<string> tempNumList;
+
+            if (numList.Count > unitCount)
+            {
+                tempNumList = numList.GetRange(0, unitCount - 1);
+                numList.RemoveRange(0, unitCount - 1);
+            }
+            else
+            {
+                tempNumList = numList.GetRange(0, numList.Count);
+                numList.Clear();
+            }
+
+            for (var i = 0; i < tempNumList.Count; i++)
+            {
+                var unitData = jpUnitMap.ElementAt(i).Value;
+                if (unitData != null)
+                {
+                    sb.Insert(0, unitData.Value.Label);
+                }
+                sb.Insert(0, tempNumList[i]);
+            }
+
+            if (numList.IsNotEmpty())
+            {
+                sb.Insert(0, lastNumData.Label);
+            }
+        } while (numList.IsNotEmpty());
+
+        return sb.ToString();
     }
 
     private static readonly SortedDictionary<int, UnitData> siUnitMap = new(intDescComparer)
@@ -46,7 +223,8 @@ public class NumericUnit
         { 3, new("K", "kilo") },
     };
 
-    private static readonly SortedDictionary<int, UnitData> jpUnitMap = new(intDescComparer)
+
+    private static readonly SortedDictionary<int, UnitData?> jpUnitMap = new()
     {
         { 68, new("無量大数", "muryoutaisuu") },
         { 64, new("不可思議", "fukashigi") },
@@ -65,26 +243,71 @@ public class NumericUnit
         { 12, new("兆", "chou") },
         { 8, new("億", "oku") },
         { 4, new("万", "man") },
+        { 0, null },
     };
 
+    private static UnitData? GetJpUnitData(int exp10)
+    {
+        for (var i = jpUnitMap.Count - 1; i > 0; i--)
+        {
+            var elm = jpUnitMap.ElementAt(i);
+            if (elm.Value != null && exp10 >= elm.Key)
+            {
+                return elm.Value!.Value;
+            }
+        }
+        return null;
+    }
+
+
+    private static BigInteger InnerBiValueProcess(BigInteger bi, int targetDigits, Func<BigInteger, BigInteger, bool> func)
+    {
+        if (targetDigits > 1)
+        {
+            var baseValue = GetBaseValue(targetDigits);
+            var halfBaseValue = baseValue / 2;
+            var mPart = bi % baseValue;
+            bi -= mPart;
+            if (func(mPart, halfBaseValue))
+            {
+                bi += baseValue;
+            }
+        }
+
+        return bi;
+    }
+
+
+    public static BigInteger GetCeilingBigInteger(BigInteger bi, int ceilDigit) => InnerBiValueProcess(bi, ceilDigit, (m, half) => m > BigInteger.Zero);
+    public static BigInteger GetRoundedBigInteger(BigInteger bi, int roundDigit) => InnerBiValueProcess(bi, roundDigit, (m, half) => m >= half);
+    public static BigInteger GetFlooringBigInteger(BigInteger bi, int floorDigit) => InnerBiValueProcess(bi, floorDigit, (m, half) => false);
+
+    
     public static int CountDigits(BigInteger bi)
     {
         return (int)Math.Ceiling(BigInteger.Log10(bi));
+    }
+
+    public static int GetBase10Exponent(BigInteger bi)
+    {
+        var dCount = CountDigits(bi);
+        return (dCount > 0) ? dCount - 1 : 0;
     }
 
 
 
     public static string GetExponentialString(BigInteger bi, int exponentDigits = 6, bool addComma = false)
     {
-        var digits = CountDigits(bi);
-        if (digits < exponentDigits)
+        var digits = GetBase10Exponent(bi);
+        if (exponentDigits < 1 || digits < exponentDigits)
         {
             return addComma ? bi.ToString("#,0") : bi.ToString();
         }
         else
         {
-            digits--;
-            var fBase = GetBaseValue(digits - 1);
+            var d = digits - 1;
+            bi = GetRoundedBigInteger(bi, d);
+            var fBase = GetBaseValue(d);
             var v = bi / fBase;
             var i = v / 10;
             var f = v % 10;
@@ -92,10 +315,10 @@ public class NumericUnit
         }
     }
 
-    public static string GetUnitString(BigInteger bi, bool addComma = false)
+    public static string GetSiUnitString(BigInteger bi, bool addComma = false)
     {
         //--- en
-        var digits = CountDigits(bi);
+        var digits = GetBase10Exponent(bi);
         var unit = "";
         var fPart = BigInteger.Zero;
 
@@ -103,10 +326,11 @@ public class NumericUnit
         {
             if (digits >= uData.Key)
             {
-                var baseValue = GetBaseValue(uData.Key - 3);
-                var reBi = bi / baseValue;
-                fPart = reBi % siUnitBase;
-                bi = reBi / siUnitBase;
+                var d = uData.Key - 1;
+                var baseValue = GetBaseValue(uData.Key);
+                bi = GetRoundedBigInteger(bi, d);
+                fPart = bi % baseValue / GetBaseValue(d);
+                bi /= baseValue;
                 unit = uData.Value.Label;
                 break;
             }
